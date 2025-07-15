252d910a47a1baba7a0901d205d1aecb78b68db0
clemo97 <lumumbaclement@gmail.com>
2025-07-15 17:34:52 +0300

    Initial Commit

---
+++ onAppear/onAppearApp.swift
@@ -0,0 +1,17 @@
+//
+//  onAppearApp.swift
+//  onAppear
+//
+//  Created by Clement Lumumba on 15/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct onAppearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ onAppearTests/onAppearTests.swift
@@ -0,0 +1,16 @@
+//
+//  onAppearTests.swift
+//  onAppearTests
+//
+//  Created by Clement Lumumba on 15/07/2025.
+//
+
+import Testing
+
+struct onAppearTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
